5c11466bda8ff7278dd7e3c3e91d73e03d976ff1
Qzwini <qzwini@gmail.com>
2020-06-02 01:36:59 +0300

    Test Get nd Post

--- app.py
+++ app.py
@@ -1,20 +1,27 @@
-from flask import Flask, jsonify, request
-app = Flask(__name__)
+from flask import Flask, jsonify, request #import objects from the Flask model
+app = Flask(__name__) #define app using Flask
 
-language = [{'name' : 'Python'}, {'name' : 'FLask'}, {'name' : 'Django'}, {'name' : 'C++'}]
+languages = [{'name' : 'Python'}, {'name' : 'FLask'}, {'name' : 'Django'}, {'name' : 'C++'}]
 
 @app.route('/', methods=['GET'])
 def test():
-    return jsonify({'message':'Test API GET'})
+	return jsonify({'message' : 'It works!'})
 
-@app.route('/lang', methods={'GET'})
+@app.route('/lang', methods=['GET'])
 def returnAll():
-    return jsonify({'language' : language})
+	return jsonify({'languages' : languages})
 
 @app.route('/lang/<string:name>', methods=['GET'])
-def returnOnly(name):
-    langs = [language for language in language if language['name'] == name]
-    return jsonify({'language' : langs[0]})
+def returnOne(name):
+	langs = [language for language in languages if language['name'] == name]
+	return jsonify({'language' : langs[0]})
+
+@app.route('/lang', methods=['POST'])
+def addOne():
+	language = {'name' : request.json["name"]}
+
+	languages.append(language)
+	return jsonify({'languages' : languages})
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
